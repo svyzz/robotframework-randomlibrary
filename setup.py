c91e9cd4498a842aa397e0f9bd0de9a1d8f77d74
Arun Venkatram <stuxnetting@gmail.com>
2013-10-03 14:16:05 +0530

    Renaming the source directory from robotframework-random to src and cleaning up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='RobotFramework-Random',
-    version='0.1.0',
+    version='0.1.1',
     author='Arun Venkatram',
     author_email='stuxnetting@gmail.com',
     packages=['robotframework-random', 'robotframework-random.test'],
