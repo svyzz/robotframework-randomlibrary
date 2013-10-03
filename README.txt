b4aa857dc90c48558277af5d26a1b80658d2a65c
Arun Venkatram <stuxnetting@gmail.com>
2013-10-04 00:33:22 +0530

    Checking in changes that let me install my distributable package through pip

--- README.txt
+++ README.txt
@@ -4,6 +4,10 @@ RobotFramework-Random
 
 This repository details a collection of **random utilities that can be used within tests written using RobotFramework**. As of this moment, there is no detailed documentation yet on the modules and keywords available, but the expectation is to fix this over time.
 
+Installation and Usage
+======================
+**pip install rf-random** should do the trick!
+
 Contributors
 ============
 * Arun Venkatram (https://github.com/svyzz)
--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
-from distutils.core import setup
-
-DESCRIPTION = """
-This repository details a collection of random utilities that can be used within tests written using RobotFramework
-"""[1:-1]
+from setuptools import setup
+from setuptools import find_packages
 
 setup(
-    name = 'robotframework-random',
-    version = '0.1.4',
-    author = 'Arun Venkatram',
-    author_email = 'stuxnetting@gmail.com',
-    packages = ['src', 'tests'],
-    url = 'https://github.com/svyzz/robotframework-random',
-    license = 'LICENSE.txt',
-    keywords = 'robotframework testing test automation random arun',
-    description = DESCRIPTION,
-    long_description = open('README.txt').read(),
-    install_requires = [
-        "requests >= 2.0.0",
-        "yolk >= 0.4.3",
-    ],
+    name='rf-random',
+    version='0.0.2',
+    author=u'Arun Venkatram',
+    author_email='stuxnetting@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/svyzz/robotframework-random',
+    license='Apache 2.0, see LICENSE.txt',
+    keywords='robotframework automation arun',
+    platforms='any',
+    description='A collection of random utilities that can be used within tests written using RobotFramework',
+    long_description=open('README.txt').read(),
+    zip_safe=False,
+    install_requires = ['robotframework', 'requests'],
 )
