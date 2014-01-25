01557d765467a59e6274dc0a25a3454583155069
Arun Venkatram <stuxnetting@gmail.com>
2014-01-25 17:38:49 +0530

    Making minor changes to the original documentation

--- src/RandomLibrary/__init__.py
+++ src/RandomLibrary/__init__.py
@@ -4,5 +4,11 @@ class RandomLibrary(RandomKeywords):
     """ RobotFramework-Random (RF-Random)is a keyword library that includes
     a bunch of random utilities that aren't found in the default libraries.
     https://github.com/svyzz/robotframework-random
+
+    We wrote a bunch of custom keywords to fit the needs of our project and
+    realized later on that this would be incredibly useful to others too.
+    This, however, is a work in progress. We intend to create pull requests and push
+    specific keywords from this library to any other applicable library when the
+    need arises.
     """
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
